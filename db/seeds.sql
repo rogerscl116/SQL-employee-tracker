a907809f15ca76302f0e0d71f9c35e3f66129ab3
charity.rogers <rogerscl116@gmail.com>
2021-07-10 11:55:49 -0500

    remove id from seeds as its not needed

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,31 +1,31 @@
 -- department seeds
-INSERT INTO department (id, name)
+INSERT INTO department (name)
 VALUES 
-    (1, "Production"),
-    (2, "Sales"),
-    (3, "Human Resouces"),
-    (4, "IT");
+    ("Production"),
+    ("Sales"),
+    ("Human Resouces"),
+    ("IT");
 
 -- role seeds
-INSERT INTO role (id, title, salary, department_id)
+INSERT INTO role (title, salary, department_id)
 VALUES 
-    (1, "Production Technician", 40000, 1),
-    (2, "Production Manager", 70000, 1),
-    (3, "Sales Specialist", 42000, 2),
-    (4, "Sales Manager", 80000, 2),
-    (5, "HR Director", 100000, 3),
-    (6, "HR Coordinator", 45000, 3),
-    (7, "IT Manager", 80000, 4),
-    (8, "IT Project Coordinator", 52000, 4);
+    ("Production Technician", 40000, 1),
+    ("Production Manager", 70000, 1),
+    ("Sales Specialist", 42000, 2),
+    ("Sales Manager", 80000, 2),
+    ("HR Director", 100000, 3),
+    ("HR Coordinator", 45000, 3),
+    ("IT Manager", 80000, 4),
+    ("IT Project Coordinator", 52000, 4);
 
 -- employee seeds
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES 
-    (1, "Leslie", "Knope", 5, null),
-    (2, "Donna", "Meagle", 6, 5),
-    (3, "Ron", "Swanson", 4, null),
-    (4, "Jerry", "Gergich", 3, 4),
-    (5, "Ann", "Perkins", 8, 7),
-    (6, "Tom", "Haverford", 7, null),
-    (7, "Andy", "Dwyer", 1, 2),
-    (8, "April", "Ludgate", 2, null);
+    ("Leslie", "Knope", 5, null),
+    ("Donna", "Meagle", 6, 5),
+    ("Ron", "Swanson", 4, null),
+    ("Jerry", "Gergich", 3, 4),
+    ("Ann", "Perkins", 8, 7),
+    ("Tom", "Haverford", 7, null),
+    ("Andy", "Dwyer", 1, 2),
+    ("April", "Ludgate", 2, null);
